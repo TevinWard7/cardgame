c2f6be23714ff1b6ea7a635cb3680b74c1fe1490
Tevin <tevinward7@gmail.com>
2021-07-31 16:00:19 -0400

    aligned sides via flexbox

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import deck from './images/deck.png'
 function App() {
   return (
     <div className="App background">
-      <div>
+
+      <h1>Game</h1>
+
+      <div className="players">
+
         <div>
-          yo
+          <h1>You</h1>
           <img src={deck} alt="card deck" height="200px" width="200px"/>
         </div>
-        <div>
 
+        <div>
+          <h1>Computer</h1>
+          <img src={deck} alt="card deck" height="200px" width="200px"/>
         </div>
+
       </div>
+
     </div>
   );
 }
